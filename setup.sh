d550cbd873e717c61e324c67caa1ab564dab481f
Akmal <magchuz@pm.me>
2021-06-09 13:17:50 +0700

    Add requirements.txt

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 # git clone "https://github.com/TheoCoombes/crawlingathome"
 # pip3 install -r crawlingathome/requirements.txt
-pip3 install ftfy regex tqdm pycld2 tfr_image cairosvg trio asks transformers datasets
+pip3 install -r requirements.txt
