cc57fa162448f6ac75799ac8af02d0ed303657aa
Akmal <magchuz@pm.me>
2021-06-13 14:57:35 +0700

    Add script to fully setup (not tested)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sudo apt install build-essential python3-dev python3-pip
+sudo apt-get update
+sudo apt-get install -y git build-essential python3-dev python3-pip python3-venv
+if [ ! -f "crawlingathome.py" ]; then
+    git clone https://github.com/Wikidepia/headless-crawlingathome
+    cd headless-crawlingathome
+    python3 -m venv venv && . venv/bin/activate
+fi
 git clone "https://github.com/TheoCoombes/crawlingathome" crawlingathome_client
 pip3 install -r crawlingathome_client/requirements.txt --no-cache-dir
 pip3 install -r requirements.txt --no-cache-dir
