2ef8f185a7ea30ddb80163fdd1742a73fc940b6d
Akmal <magchuz@pm.me>
2021-06-13 14:41:32 +0700

    Add needed library for cld2

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt install build-essential python3-dev python3-pip
 git clone "https://github.com/TheoCoombes/crawlingathome" crawlingathome_client
 pip3 install -r crawlingathome_client/requirements.txt --no-cache-dir
 pip3 install -r requirements.txt --no-cache-dir
