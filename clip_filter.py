4e80195e5bdd08ebef3c89b2a2606a3d6c31d657
Akmal <magchuz@pm.me>
2021-06-13 13:26:57 +0700

    Only quantize if cuda is not available

--- clip_filter.py
+++ clip_filter.py
@@ -11,12 +11,10 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 class CLIP:
     def __init__(self):
         self.model, self.preprocess = clip.load("ViT-B/32", device=device, jit=False)
-        self.model = torch.quantization.quantize_dynamic(self.model, dtype=torch.qint8)
+        if device == "cpu":
+            self.model = torch.quantization.quantize_dynamic(self.model, dtype=torch.qint8)
         self.tokenize = clip.tokenize
         self.cosine_similarity = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
-        self.categories = ["neutral","selfie", "illustration, drawing", "toys, play, kids, children", "teddy bear, puppet", "animal, bird, mammal, insect" "fashion, clothes", "logo, commercial, ad, advertisement", "drawing, painting","anime, cartoon","comedy, fun","romance, love story","thriller, suspense, crime story","action, action movie", "horror, monster movie", "documentary", "news, journalism", "entertainment", "talk show", "porn, sex, sperm, nipples, breats, tits, boops, penis, dick, cock, clitoris, vagina, fuck, lust, horny, sexual, lick, licking",  "porn, sex, sperm, nipples", "porn, sex, sperm, penis, dick, cock", "nipples, breats, tits, boops, sexy", "penis, dick, cock", "clitoris, vagina", "sex, fuck, lust, horny, sexual, lick, licking", "porn, sex, sexy","sexy, hot","sperm, skin","lust, horny, sexual","lick, licking, body", "anime, hentai, sexy", "cartoon, sexy, sex", "hentai", "anime, sexy, breasts", "hentai"]
-        self.underaged_categories = ["teenager, teen", "kid, child, teenager, teen, baby or toddler, underaged, little girl, little boy", "kid, child, little girl, little boy", "baby, toddler","adult, woman, man, grownup, grown person,full-aged of legal age","full-aged, of legal age, adult","woman, man","adult, woman, man, grownup, grown person,full-aged of legal age"]
-        self.animal_categories = ["lifeless object, thing", "thing, object", "material", "furniture","wall", "house", "tree", "wood","ground","industry", "table", "bed", "tool", "dress, clothes", "door", "chair", "rock, stone", "human", "man", "woman", "man, woman", "animal","cat","dog", "cow", "pig", "goat", "sheep", "elephant", "horse", "horse, elephant, pig, dog, cat, sheep, goat, animal", "life", "wildlife"]
 
     def load_img(self, path):
         image = Image.open(path)
@@ -33,16 +31,12 @@ class CLIP:
         texts = [self.tokenize(text[:77]).to(device) for text in batch["TEXT"]]
 
         with torch.no_grad():
-            batch["image_features"] = self.model.encode_image(
+            image_features = self.model.encode_image(
                 torch.cat([x.float() for x in images])
             )
-            batch["text_features"] = self.model.encode_text(
-                torch.cat([x for x in texts])
-            )
+            text_features = self.model.encode_text(torch.cat([x for x in texts]))
 
-        for image_feat, text_feat in zip(
-            batch["image_features"], batch["text_features"]
-        ):
+        for image_feat, text_feat in zip(image_features, text_features):
             similarity.append(
                 float(
                     self.cosine_similarity(
@@ -53,8 +47,7 @@ class CLIP:
             )
 
         batch["similarity"] = similarity
-        batch["image_features"] = batch["image_features"].detach().cpu().numpy()
-        batch["text_features"] = batch["text_features"].detach().cpu().numpy()
+        batch["image_features"] = image_features.detach().cpu().numpy()
         return batch
 
     def preprocess_images(self, df):
