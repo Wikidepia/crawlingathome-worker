8eacd9b24e74deec408027e39ffc3a300b30608d
Akmal <magchuz@pm.me>
2021-07-03 07:45:55 +0700

    Lower connections

--- setup.sh
+++ setup.sh
@@ -13,9 +13,8 @@ pip3 install -r requirements.txt --no-cache-dir
 if [ ! "$1" == "tpu" ]; then
     pip3 install tensorflow tfr_image datasets --no-cache-dir
     pip3 install git+https://github.com/Wikidepia/CLIP --no-cache-dir
+    yes | pip3 uninstall pillow
+    CC="cc -mavx2" pip3 install -U --force-reinstall pillow-simd
 fi
 ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_cah -q -P ""
 sed -i -e "s/<<your_ssh_public_key>>/$(sed 's:/:\\/:g' ~/.ssh/id_cah.pub)/" cloud-config.yaml
-
-yes | pip3 uninstall pillow
-CC="cc -mavx2" pip3 install -U --force-reinstall pillow-simd
