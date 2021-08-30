7965812b756cf8f7ee2ef64514e40877043d2f0d
Akmal <magchuz@pm.me>
2021-08-30 18:06:33 +0700

    Skip lookup if host already known as non existent

--- shim.py
+++ shim.py
@@ -1,9 +1,17 @@
 import socket
 from functools import lru_cache
 
+error_host = set()
 orig_getaddrinfo = socket.getaddrinfo
+
 @lru_cache(maxsize=50_000)
 def shim_getaddrinfo(host, port, family=0, socktype=0, proto=0, flags=0):
-    return orig_getaddrinfo(host, port, socket.AF_INET, socktype, proto, flags)
+    try:
+        if host in error_host:
+            raise socket.gaierror
+        return orig_getaddrinfo(host, port, socket.AF_INET, socktype, proto, flags)
+    except socket.gaierror as e:
+        if "Name or service not known" in str(e):
+            error_host.add(host)
 
 socket.getaddrinfo = shim_getaddrinfo
